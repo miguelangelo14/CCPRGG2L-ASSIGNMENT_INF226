51f7467db8d73519ff24d9154c7560251eb9f4f0
Migue Angelo D. Enrico <122077301+miguelangelo14@users.noreply.github.com>
2023-02-10 14:23:18 +0800

    Add files via upload

---
+++ exercise 5/Father.java
@@ -0,0 +1,9 @@
+public class Father {
+
+    protected String surname = "Enrico";
+    
+    protected void showSurname() {
+        System.out.println("My family name is: " + surname);
+    }
+    
+}
---
+++ exercise 5/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    private String name = "Miguel";
+
+    public String getName() {
+        return this.name;
+
+
+    }
+
+    public void setName(String personName) {
+
+    }
+
+}
